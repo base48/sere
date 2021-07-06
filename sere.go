ba185bd61d1087fd1e874d267a191591bf152512
skirit <skirit@dump.cz>
2021-07-07 01:07:59 +0200

    add second sensor

--- sere.go
+++ sere.go
@@ -8,27 +8,37 @@ import(
 
 	"github.com/julienschmidt/httprouter"
 	bh1750 "github.com/d2r2/go-bh1750"
+	bsbmp "github.com/d2r2/go-bsbmp"
 	i2c "github.com/d2r2/go-i2c"
 )
 
 type(
 	Data struct{
-		Ligh string `json:"ligh"`
-		Temp string `json:"temp"`
-		Pres string `json:"pres"`
+		Ligh string `json:"illuminance"`
+		Temp string `json:"temperature"`
+		Pres string `json:"pressure"`
+		Humi string `json:"humidity"`
 	}
 )
 
 func GetSen(){
-	i2c, _ := i2c.NewI2C(0x23, 1)
-	defer i2c.Close()
-	sensor := bh1750.NewBH1750()
-	sensor.ChangeSensivityFactor(i2c, 255)
-	resolution := bh1750.HighestResolution
+	i2cl, _ := i2c.NewI2C(0x23, 1) // light sen
+	i2co, _ := i2c.NewI2C(0x76, 1) // temp, pres, humi sen
+	defer i2cl.Close()
+	defer i2co.Close()
+	sensorl := bh1750.NewBH1750()
+	sensorl.ChangeSensivityFactor(i2cl, 255)
+	sensoro, _ := bsbmp.NewBMP(bsbmp.BME280, i2co)
 
 	for{
-		amb, _ := sensor.MeasureAmbientLight(i2c, resolution)
-		datag.Ligh = fmt.Sprintf("%v", amb)
+		a, _ := sensorl.MeasureAmbientLight(i2cl, bh1750.HighestResolution)
+		datag.Ligh = fmt.Sprintf("%v", a)
+		t, _ := sensoro.ReadTemperatureC(bsbmp.ACCURACY_STANDARD)
+		datag.Temp = fmt.Sprintf("%v", t)
+		p, _ := sensoro.ReadPressurePa(bsbmp.ACCURACY_STANDARD)
+		datag.Pres = fmt.Sprintf("%v", p)
+		_, h, _ := sensoro.ReadHumidityRH(bsbmp.ACCURACY_STANDARD)
+		datag.Humi = fmt.Sprintf("%v", h)
 		time.Sleep(1000 * time.Millisecond)
 	}
 }
@@ -42,6 +52,7 @@ func main(){
 			Ligh:	datag.Ligh,
 			Temp:	datag.Temp,
 			Pres:	datag.Pres,
+			Humi:	datag.Humi,
 		}
 		dataj, _ := json.Marshal(data)
 
@@ -51,5 +62,5 @@ func main(){
 	})
 
 	go GetSen()								// sensors thread
-	http.ListenAndServe("0.0.0.0:3000", r)	// server thread
+	http.ListenAndServe("0.0.0.0:10000", r)	// server thread
 }
